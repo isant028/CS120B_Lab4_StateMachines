0ab22d6c19d3ba727bdec789553312e5e31619aa
Isabella Santiago <isant028@wch136-24.cs.ucr.edu>
2021-04-18 11:45:38 -0700

    part 1

--- source/main.c
+++ source/main.c
@@ -1,7 +1,7 @@
 /*	Author: isant028
  *  Partner(s) Name: 
  *	Lab Section:
- *	Assignment: Lab #  Exercise #
+ *	Assignment: Lab 4  Exercise 1
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
@@ -11,13 +11,89 @@
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
 #endif
+enum LED_STATES {LED_Start, LED_On, LED_Button, LED_On2, LED_Button2} LED_STATE;
+void LED_Tick() {
+	switch(LED_STATE) {
+		case LED_Start:
+			LED_STATE = LED_On;
+			break;
+		case LED_On:
+			if ((PINA & 0x01) == 0x01) {
+				LED_STATE = LED_On;
+			}
+			else {
+				LED_STATE = LED_Button;
+			}
+			break;
+		
+		case LED_Button:
+			if ((PINA & 0x01) == 0x01) {
+                 LED_STATE = LED_On2;
+                        }
+            else {
+                LED_STATE = LED_Button;
+                }
+                break;
+	
+		case LED_On2:
+			if ((PINA & 0x01) == 0x01) {
+				LED_STATE = LED_On2;
+			}
+			else {
+				LED_STATE = LED_On2Release;
+			}
+			break;
+		
+		case LED_Button2:
+			if ((PINA & 0x01) == 0x01) {
+				LED_STATE = LED_On;
+			}
+			else {
+				LED_STATE = LED_Button2;
+			}
+			break;
+
+		default:
+			LED_STATE = LED_Start;
+			break;
+	}
+	switch(LED_STATE) {
+		
+		case LED_Start:
+			 
+			PORTB = 0x01;//B0 = 1; B1 =0;
+			break;
+		
+		case LED_On:
+			
+            PORTB = 0x01;//B0 = 1; B1 =0; 
+             break;
+		
+		case LED_Button:
+			break;
+		
+		case LED_On2:
+			
+            PORTB = 0x02;//B0 = 0 B1 =1; 
+            break;
+		
+		case LED_Button2; 
+			break;
+		
+		default:
+			PORTB = 0x01;
+			break;
+	}
+}
 
 int main(void) {
-    /* Insert DDR and PORT initializations */
-
-    /* Insert your solution below */
-    while (1) {
-
-    }
+    DDRA = 0x00; PORTA = 0xFF;
+    DDRB = 0xFF; PORTB = 0x00;
+    
+	while (1) {
+	LED_Tick();
+	}
     return 1;
 }
+	
+
